f463f2da2e0c328a1822c40671975885b9372155
Michał Wołek <m.wolek@nebucode.com>
2023-12-05 21:56:42 +0100

    day 3 of advent of code (2 days late), code without refactoring

---
+++ src/advent_of_code/day_three/gear_ratios.rs
@@ -0,0 +1,261 @@
+use std::collections::HashSet;
+
+use super::file_reader::FileReader;
+
+type Grid = Vec<Vec<char>>;
+
+struct GridParser {}
+
+impl GridParser {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn parse(&self, string: String) -> Grid {
+        string
+            .split("\n")
+            .map(|line| line.chars().collect())
+            .collect()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct NumberData {
+    number: i32,
+    adjacent_chars: Vec<char>,
+}
+
+struct GridNumberLocator {
+    grid_parser: GridParser,
+    directions: Vec<(i32, i32)>,
+}
+
+impl GridNumberLocator {
+    fn new() -> Self {
+        Self {
+            grid_parser: GridParser::new(),
+            directions: vec![
+                (-1, -1),
+                (-1, 0),
+                (-1, 1),
+                (0, 1),
+                (1, 1),
+                (1, 0),
+                (1, -1),
+                (0, -1),
+            ],
+        }
+    }
+
+    fn get_char_at(grid: &Grid, i: usize, j: usize) -> Option<char> {
+        match grid.get(i) {
+            None => None,
+            Some(row) => match row.get(j) {
+                None => None,
+                Some(&c) => match c.is_digit(10) || c.to_string() == String::from(".") {
+                    true => None,
+                    false => Some(c),
+                },
+            },
+        }
+    }
+
+    fn consume_temporary_state(
+        grid: &Grid,
+        digits: &String,
+        hashset: &HashSet<(usize, usize)>,
+    ) -> Option<NumberData> {
+        if digits.len() == 0 {
+            return None;
+        }
+
+        let adjacent_chars: Vec<char> = hashset
+            .iter()
+            .map(|(i, j)| Self::get_char_at(grid, *i, *j))
+            .filter(|c| c.is_some())
+            .map(|c| c.unwrap())
+            .collect();
+        let number: i32 = digits.parse().unwrap();
+
+        Some(NumberData {
+            number,
+            adjacent_chars,
+        })
+    }
+
+    fn analyze_grid(&self, string: String) -> Vec<NumberData> {
+        let grid = self.grid_parser.parse(string);
+
+        let mut hashset: HashSet<(usize, usize)> = HashSet::new();
+        let mut digits = String::from("");
+
+        let mut number_data_vec: Vec<NumberData> = Vec::new();
+
+        grid.iter().enumerate().for_each(|(i, row)| {
+            row.iter()
+                .enumerate()
+                .for_each(|(j, &c)| match c.is_digit(10) {
+                    true => {
+                        self.directions.iter().for_each(|(x, y)| {
+                            let x = i as i32 + x;
+                            let y = j as i32 + y;
+                            if x >= 0 && y >= 0 {
+                                hashset.insert((x as usize, y as usize));
+                            }
+                        });
+                        digits.push(c);
+                    }
+                    false => match Self::consume_temporary_state(&grid, &digits, &hashset) {
+                        None => (),
+                        Some(number_data) => {
+                            number_data_vec.push(number_data);
+
+                            hashset = HashSet::new();
+                            digits = String::from("");
+                        }
+                    },
+                });
+
+            match Self::consume_temporary_state(&grid, &digits, &hashset) {
+                None => (),
+                Some(number_data) => {
+                    number_data_vec.push(number_data);
+
+                    hashset = HashSet::new();
+                    digits = String::from("");
+                }
+            }
+        });
+
+        number_data_vec
+    }
+}
+
+struct PartNumbersCalculator {}
+
+impl PartNumbersCalculator {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn calculate_sum(&self, numbers_data: &Vec<NumberData>) -> i32 {
+        numbers_data.iter().fold(0, |acc, number_data| {
+            acc + match number_data.adjacent_chars.len() > 0 {
+                true => number_data.number,
+                false => 0,
+            }
+        })
+    }
+}
+
+pub struct GearRatios {
+    numbers_data_vec: Vec<NumberData>,
+    file_reader: FileReader,
+    parts_number_calculator: PartNumbersCalculator,
+    grid_number_locator: GridNumberLocator,
+    grid_parser: GridParser,
+}
+
+impl GearRatios {
+    pub fn new() -> Self {
+        Self {
+            numbers_data_vec: vec![],
+            file_reader: FileReader::new(),
+            parts_number_calculator: PartNumbersCalculator::new(),
+            grid_parser: GridParser::new(),
+            grid_number_locator: GridNumberLocator::new(),
+        }
+    }
+
+    pub fn read_from_file(&mut self, filename: &str) {
+        let string = self.file_reader.read(filename);
+        let numbers_data_vec = self.grid_number_locator.analyze_grid(string);
+        self.numbers_data_vec = numbers_data_vec;
+    }
+
+    pub fn calculatr_sum_of_part_numbers(&self) -> i32 {
+        self.parts_number_calculator
+            .calculate_sum(&self.numbers_data_vec)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::advent_of_code::day_three::gear_ratios::{
+        GearRatios, GridNumberLocator, GridParser, NumberData, PartNumbersCalculator,
+    };
+
+    #[test]
+    fn parses_string_to_grid() {
+        let string_from_file = String::from("467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..");
+        let grid_parser = GridParser::new();
+
+        let grid = grid_parser.parse(string_from_file);
+
+        assert_eq!(grid.len(), 10);
+        assert_eq!(grid.get(0).unwrap().len(), 10);
+    }
+
+    #[test]
+    fn finds_number_and_its_adjacent_characters() {
+        let grid_number_locator = GridNumberLocator::new();
+
+        let numbers_data = grid_number_locator.analyze_grid("467....114\n11.*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..".to_string());
+
+        let first_item = numbers_data.get(0).unwrap();
+        let expected = NumberData {
+            number: 467,
+            adjacent_chars: "*".chars().collect(),
+        };
+        assert_eq!(*first_item, expected);
+    }
+
+    #[test]
+    fn number_at_the_begining_of_a_line_is_not_a_continuation_of_a_number_ending_on_previous_line()
+    {
+        let grid_number_locator = GridNumberLocator::new();
+
+        let numbers_data = grid_number_locator.analyze_grid("467....114\n11.*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..".to_string());
+
+        let first_item = numbers_data.get(1).unwrap();
+        let expected = NumberData {
+            number: 114,
+            adjacent_chars: vec![],
+        };
+        assert_eq!(*first_item, expected);
+    }
+
+    #[test]
+    fn extracts_random_number_from_somewhere_in_the_middle() {
+        let grid_number_locator = GridNumberLocator::new();
+
+        let numbers_data = grid_number_locator.analyze_grid("467....114\n11.*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..".to_string());
+
+        let expected = NumberData {
+            number: 664,
+            adjacent_chars: "$".chars().collect(),
+        };
+        assert!(numbers_data.contains(&expected));
+    }
+
+    #[test]
+    fn calculates_sum_of_part_numbers() {
+        let grid_number_locator = GridNumberLocator::new();
+        let numbers_data = grid_number_locator.analyze_grid("467....114\n11.*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..".to_string());
+        let part_numbers_calculator = PartNumbersCalculator::new();
+
+        let sum = part_numbers_calculator.calculate_sum(&numbers_data);
+
+        assert_eq!(sum, 4361);
+    }
+
+    #[test]
+    fn calculates_sum_of_part_numbers_from_file_input() {
+        let mut gear_ratios = GearRatios::new();
+        gear_ratios.read_from_file("./src/advent_of_code/day_three/test-input.txt");
+
+        let sum = gear_ratios.calculatr_sum_of_part_numbers();
+
+        assert_eq!(sum, 4361);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
+use advent_of_code::day_three::gear_ratios::GearRatios;
+
 // mod currency;
 // mod codewars;
 mod advent_of_code;
 
-use advent_of_code::day_two::cube_conundrum_2::CubeConundrum;
-
 fn main() {
-    let mut cube_conundrum = CubeConundrum::new();
-    cube_conundrum.read_games_from_file("./src/advent_of_code/day_two/input.txt");
-    cube_conundrum.insert_cubes_into_bag(12, "red");
-    cube_conundrum.insert_cubes_into_bag(13, "green");
-    cube_conundrum.insert_cubes_into_bag(14, "blue");
+    let mut gear_ratios = GearRatios::new();
+    gear_ratios.read_from_file("./src/advent_of_code/day_three/input.txt");
 
-    let sum_of_ids = cube_conundrum.get_sum_of_valid_game_ids();
-    let sum_of_powers = cube_conundrum.get_sum_of_game_powers();
+    let sum = gear_ratios.calculatr_sum_of_part_numbers();
 
-    println!("Sum of ids: {}", sum_of_ids);
-    println!("Sum of powers: {}", sum_of_powers);
+    println!("{sum}")
 }
