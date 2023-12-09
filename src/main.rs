f6b385e9989a59f600bb8e8f0c4d740c6d034e70
Michał Wołek <m.wolek@nebucode.com>
2023-12-09 19:12:50 +0100

    Advent of code day 9 part II

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use crate::advent_of_code::day_nine::mirage_maintenance::{
-    get_sum_of_next_numbers_from_file, get_sum_of_previous_numbers_from_file,
-};
-
 // mod currency;
 // mod codewars;
 mod advent_of_code;
 
-fn main() {
-    let sum_of_previous_numbers =
-        get_sum_of_previous_numbers_from_file("./src/advent_of_code/day_nine/input.txt");
-
-    assert_eq!(sum_of_previous_numbers, 114);
-}
+fn main() {}
